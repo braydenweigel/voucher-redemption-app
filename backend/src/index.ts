9f5e6dc71a5c52fd1a75755be953cc9974f73a3c
braydenweigel <braydenweigel7@gmail.com>
2026-02-23 17:57:24 -0800

    initial setup

---
+++ backend/src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+import morgan from 'morgan'
+import router from './router/index.js'
+
+
+const app = express()
+const port = 3000
+
+app.use(morgan('dev'))
+app.use(express.json())
+app.use('/', router)
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
