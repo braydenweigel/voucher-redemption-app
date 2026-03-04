e6963409b36a792442f7a883a0fb4d3f15e35524
braydenweigel <braydenweigel7@gmail.com>
2026-03-03 21:24:55 -0800

    voucher redemption by scanning

--- mobile/voucher-redemption/src/app/(tabs)/index.tsx
+++ mobile/voucher-redemption/src/app/(tabs)/index.tsx
@@ -2,13 +2,17 @@ import { Stack } from "expo-router";
 import { StyleSheet, View, Text, FlatList, TextInput, Pressable, Alert } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { ScanBarcode } from 'lucide-react-native'
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { CameraView, CameraType, useCameraPermissions } from "expo-camera"
 import { supabase } from "@/lib/supabase/supabase";
+
 
 export default function HomePage(){
     const [voucher, setVoucher] = useState('')
     const [permission, requestPermission] = useCameraPermissions()
+    const [scanned, setScanned] = useState(true)
+    const [barcodeData, setBarcodeData] = useState<string | null>(null)
+    const scanLock = useRef(false)
 
     if (!permission){
         return <View/>
@@ -22,23 +26,17 @@ export default function HomePage(){
         )
     }
 
-    const handleScan = () => {
-        console.log("SCAN BUTTON CLICKED")
-    }
-
-    const handleRedeem = async () => {
-        console.log("REDEEM BUTTON CLICKED: Voucher: ", voucher)
-      
+    async function verifyVoucher(id: string){
         const { data, error } = await supabase
-            .from('vouchers')
-            .select()
-            .eq("voucherid", voucher)
-            .limit(1)
-            .single()
-
+                .from('vouchers')
+                .select()
+                .eq("voucherid", id)
+                .limit(1)
+                .single()
+        
         if (error){
             Alert.alert("Voucher does not exist!")
-            return 
+            return
         }
 
         if (data.redeemed){
@@ -46,10 +44,22 @@ export default function HomePage(){
             return
         }
 
+        Alert.alert(
+            "Redeem Voucher?", 
+            undefined, 
+            [
+                { text: "Cancel", style: "cancel" },
+                { text: "Redeem", onPress: () => redeemVoucher(id) }
+            ]
+        )
+        
+    }
+
+    async function redeemVoucher(id: string){
         const { error: updateError } = await supabase
             .from('vouchers')
             .update({ redeemed: true })
-            .eq('voucherid', data.voucherid)
+            .eq('voucherid', id)
 
         if (updateError){
             Alert.alert("Error redeeming voucher!")
@@ -57,12 +67,46 @@ export default function HomePage(){
         }
 
         Alert.alert("Voucher Redeemed!")
-       
+        setVoucher('')      
     }
+
+    const handleScan = () => {
+        console.log("SCAN BUTTON CLICKED")
+        scanLock.current = false
+        setScanned(false)
+    }
+
+    const handleRedeem = async () => {
+        console.log("REDEEM BUTTON CLICKED: Voucher: ", voucher)
+        verifyVoucher(voucher)
+        
+    }
+
+    const handleBarcodeScanned = ({ type, data }: { type: string; data: string }) => {
+        if (scanLock.current) return 
+
+        scanLock.current = true
+        setScanned(true)
+        setBarcodeData(data)
+        setVoucher(data)
+
+        console.log("Barcode type:", type)
+        console.log("Barcode data:", data)
+
+        verifyVoucher(data)
+    }
+
+
 
     return (
         <SafeAreaView style={styles.page}>
-            <CameraView style={{flex: 1}} facing="back" barcodeScannerSettings={{barcodeTypes: ["code128"]}} onBarcodeScanned={handleRedeem}/>
+            {!scanned && <CameraView 
+                style={{flex: 1}} 
+                facing="back" 
+                barcodeScannerSettings={{barcodeTypes: ["code128"]}} 
+                onBarcodeScanned={!scanLock.current ? handleBarcodeScanned : undefined}
+            />}
+
             <View style={styles.voucherInput}>
                 <TextInput placeholder="Voucher ID" value={voucher} onChangeText={t => setVoucher(t)}/>
                 <Pressable onPress={handleScan}><ScanBarcode color={"#000000"}/></Pressable> 
